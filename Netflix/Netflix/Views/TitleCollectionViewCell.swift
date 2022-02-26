7e5b72fb7bdd0f0f4117386d90b8c459cfd56126
manigorman <immanakov@gmail.com>
2022-02-27 00:06:46 +0300

    Updated TtileCollectionViewCell

--- Netflix/Netflix/Views/TitleCollectionViewCell.swift
+++ Netflix/Netflix/Views/TitleCollectionViewCell.swift
@@ -9,4 +9,38 @@ import UIKit
 
 class TitleCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    public func configure(with model: String) {
+        guard let url = URL(string: model) else {return}
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url) else {return}
+            DispatchQueue.main.async {
+                self.posterImageView.image = UIImage(data: data)
+            }
+        }
+    }
+    
 }
